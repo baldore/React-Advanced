03524571f17f55b0d3d4e5256ea42470dd46712f
baldore <acornssn@gmail.com>
2018-10-26 16:14:43 -0500

    Add example for mutation and query

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, ctx, info) {
+    const newDog = { name: args.name };
+    global.dogs = global.dogs || [];
+    global.dogs.push(newDog);
+
+    return newDog;
+  },
+};
 
 module.exports = mutations;
